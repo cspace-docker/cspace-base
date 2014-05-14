33b7a20c260b045453f916b0473cebcce00ccff5
Aron Roberts <aron@socrates.berkeley.edu>
2014-05-13 18:02:10 -0700

    Changed HOME to USER_HOME, per Richard's and Aron's testing.

--- git-cspace-src.sh
+++ git-cspace-src.sh
@@ -10,8 +10,8 @@ echo $CSPACE_USER_PASSWORD$'\n'$CSPACE_USER_PASSWORD$'\n' | passwd $CSPACE_USERN
 # Install git, create a 'src' directory and clone the CollectionSpace repos
 #
 apt-get install -y git
-cd $HOME/$CSPACE_USERNAME && mkdir src
-cd $HOME/$CSPACE_USERNAME/src && git clone https://github.com/collectionspace/ui.git
-chown -R $CSPACE_USERNAME $HOME/$CSPACE_USERNAME
+cd $USER_HOME/$CSPACE_USERNAME && mkdir src
+cd $USER_HOME/$CSPACE_USERNAME/src && git clone https://github.com/collectionspace/ui.git
+chown -R $CSPACE_USERNAME $USER_HOME/$CSPACE_USERNAME
 
 exit 0
