073935d3e164850097cccce114e089420312c489
Aron Roberts <aron@socrates.berkeley.edu>
2014-07-08 17:39:23 -0700

    Experiment with adding apt-get update prior to each install command.

--- git-cspace-src.sh
+++ git-cspace-src.sh
@@ -9,7 +9,7 @@ echo $CSPACE_USER_PASSWORD$'\n'$CSPACE_USER_PASSWORD$'\n' | passwd $CSPACE_USERN
 #
 # Install git, create a 'src' directory and clone the CollectionSpace repos
 #
-apt-get install -y git
+apt-get update && apt-get install -y git
 cd $USER_HOME/$CSPACE_USERNAME && mkdir src
 cd $USER_HOME/$CSPACE_USERNAME/src && git clone https://github.com/collectionspace/application.git
 cd $USER_HOME/$CSPACE_USERNAME/src && git clone https://github.com/collectionspace/services.git
--- install-tool-dependencies.sh
+++ install-tool-dependencies.sh
@@ -1,35 +1,43 @@
 #!/bin/bash
 
 #
+# Install the software-properties-common package.
+#
+# This installs, among other things, the
+# 'add-apt-repository' command, which simplifies
+# adding configuration files for additional
+# APT package repository.
+#
+apt-get update && apt-get install -y software-properties-common
+
+#
 # Install Oracle/Sun JDK 7
 #
-apt-get install -y software-properties-common
 add-apt-repository -y ppa:webupd8team/java
-apt-get -y update
 echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
-apt-get -y install oracle-jdk7-installer
+apt-get update && apt-get -y install oracle-jdk7-installer
 update-alternatives --display java
 # Set Oracle JDK 7 as the system's default Java
-apt-get install oracle-java7-set-default
+apt-get update && apt-get install oracle-java7-set-default
 
 #
 # Install wget
 #
-apt-get install -y wget
+apt-get update && apt-get install -y wget
 
 #
 # Install Maven and Ant
 #
-apt-get install -y maven ant
+apt-get update && apt-get install -y maven ant
 
 #
 # Install the PostgreSQL client and FTP client
 #
-apt-get install -y postgresql-client ftp
+apt-get update && apt-get install -y postgresql-client ftp
 
 #
 # Install Nuxeo dependencies
 #
-apt-get install -y imagemagick
+apt-get update && apt-get install -y imagemagick
 
 exit 0
